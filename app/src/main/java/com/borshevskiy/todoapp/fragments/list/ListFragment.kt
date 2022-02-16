cc37a21d0a1217bba0af05938c10647ae39f69a0
borshevskiy <sk8er1001@mail.ru>
2022-02-16 11:37:32 +0600

    Display Data in Recyclerview

--- app/src/main/java/com/borshevskiy/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/borshevskiy/todoapp/fragments/list/ListFragment.kt
@@ -3,8 +3,13 @@ package com.borshevskiy.todoapp.fragments.list
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.borshevskiy.todoapp.R
+import com.borshevskiy.todoapp.data.viewmodel.SharedViewModel
+import com.borshevskiy.todoapp.data.viewmodel.ToDoViewModel
 import com.borshevskiy.todoapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
@@ -12,12 +17,23 @@ class ListFragment : Fragment() {
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var toDoViewModel: ToDoViewModel
+    private val mAdapter by lazy { ListAdapter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        toDoViewModel = ViewModelProvider(requireActivity())[ToDoViewModel::class.java]
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentListBinding.inflate(inflater,container,false)
-
+        setupRecyclerView()
+        toDoViewModel.getAllData.observe(viewLifecycleOwner, Observer {
+            mAdapter.setData(it)
+        })
         with(binding) {
             floatingActionButton.setOnClickListener {
                 findNavController().navigate(R.id.action_listFragment_to_addFragment)
@@ -37,5 +53,12 @@ class ListFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerView.apply {
+            adapter = mAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 }
